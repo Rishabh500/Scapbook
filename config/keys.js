c434e2c0e446775607a6a58f1800ade189fb1d98
Jain <rishabh.b.jain@capgemini.com>
2017-10-29 21:14:33 +0530

    google oauth

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:'mongodb://Rishabh Jain:ilovemyindia1234@ds237735.mlab.com:37735/story-book',
+  googleClientID:'785766672525-b7p9uu6ugjvvoius2ou0tjfuq1m02trg.apps.googleusercontent.com',
+  googleClientSecret:'FfzI6ozEHTWtrO_efxolV3ee',
+  
+  }
